186e9ead1e439db9c5442dc312cac786a64153cb
QuangNV <quang@asto-system.com>
2018-11-05 16:13:01 +0700

    xxx

--- install-jgrep.sh
+++ install-jgrep.sh
@@ -3,5 +3,5 @@ java -version || sudo yum install java 2>/dev/null || sudo apt-get install java 
 #download 
 wget https://raw.githubusercontent.com/nvq247/jgrep/master/jgrep.jar /.jgrep.jar || curl https://raw.githubusercontent.com/nvq247/jgrep/master/jgrep.jar -o /.jgrep.jar 
 alias jgrep="java -jar /.jgrep.jar"  
-echo "" >> ~/.bash_profile  && echo "alias jgrep='java -jar /.jgrep.jar'" >> ~/.bash_profile  && source ~/.bash_profile
-echo "" >> ~/.profile && echo "alias jgrep='java -jar /.jgrep.jar'" >> ~/.profile && source ~/.profile
+echo " " >> ~/.bash_profile  && echo "alias jgrep=\"java -jar /.jgrep.jar\"" >> ~/.bash_profile  && source ~/.bash_profile
+echo " " >> ~/.profile && echo "alias jgrep=\"java -jar /.jgrep.jar\"" >> ~/.profile && source ~/.profile
